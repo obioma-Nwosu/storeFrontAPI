9997a7c0f9afd4c8975384483a830be59b36ced2
obi <obioma.nwosu@fau.de>
2022-02-18 17:16:08 +0100

    Setup and initilization

---
+++ tags/models.py
@@ -0,0 +1,18 @@
+from re import M
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+# Create your models here.
+
+
+class Tags(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    # what tag is applied to what model
+    tag = models.ForeignKey(Tags, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
